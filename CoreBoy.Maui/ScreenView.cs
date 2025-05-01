9735fc8502932834a2f2c5da6c70fda7b6755fba
Pedro Jesus <pedrojesus@Pedros-MacBook-Pro.local>
2025-05-01 20:28:37 -0300

    improve maui code

--- CoreBoy.Maui/ScreenView.cs
+++ CoreBoy.Maui/ScreenView.cs
@@ -29,40 +29,69 @@ sealed partial class ScreenView : SKCanvasView, IDisplay
 		var min = Math.Min(widthConstraint, heightConstraint);
 		return new(min, min);
 	}
-
 	public void PutDmgPixel(int color)
 	{
-		var pixel = currentPixel++;
-		var x = pixel % DisplayWidth;
-		var y = pixel / DisplayWidth;
-
-		bitmap.SetPixel(x, y, Colors[color]);
-
-		currentPixel %= DisplayLength;
+		var x = currentPixel % DisplayWidth;
+		var y = currentPixel / DisplayWidth - 1;
+    
+		if (y >= 0 && y < DisplayHeight && x >= 0 && x < DisplayWidth)
+		{
+			bitmap.SetPixel(x, y, Colors[color]);
+		}
+    
+		currentPixel = (currentPixel + 1) % DisplayLength;
 	}
-	int maxR;
+	// public void PutDmgPixel(int color)
+	// {
+	// 	var pixel = currentPixel++;
+	// 	var x = pixel % DisplayWidth;
+	// 	// var y = pixel / DisplayWidth - 1;
+	// 	var y = pixel / DisplayWidth ;
+	// 	bitmap.SetPixel(x, y, Colors[color]);
+	//
+	// 	currentPixel %= DisplayLength;
+	// }
 	public void PutColorPixel(int gbcRgb)
 	{
 		var r = ((gbcRgb >> 0) & 0x1f) << 3;
 		var g = ((gbcRgb >> 5) & 0x1f) << 3;
 		var b = ((gbcRgb >> 10) & 0x1f) << 3;
-		maxR = Math.Max(r, maxR);
-		var pixel = currentPixel++;
-		var x = pixel % DisplayWidth;
-		var y = pixel / DisplayWidth;
-
-		if (y >= 0 && y < DisplayHeight)
+    
+		var x = currentPixel % DisplayWidth;
+		var y = currentPixel / DisplayWidth;
+		y+=1;
+		
+		if (y >= 0 && y < DisplayHeight && x >= 0 && x < DisplayWidth)
 		{
 			bitmap.SetPixel(x, y, new SKColor((byte)r, (byte)g, (byte)b));
 		}
-		else
-		{
-			_ = 1;
-		}
-			//bitmap.SetPixel(x, y, new SKColor((byte)r, (byte)g, (byte)b));
-
-			currentPixel %= DisplayLength;
+    
+		currentPixel = (currentPixel + 1) % DisplayLength;
 	}
+	int maxR;
+	// public void PutColorPixel(int gbcRgb)
+	// {
+	// 	var r = ((gbcRgb >> 0) & 0x1f) << 3;
+	// 	var g = ((gbcRgb >> 5) & 0x1f) << 3;
+	// 	var b = ((gbcRgb >> 10) & 0x1f) << 3;
+	// 	maxR = Math.Max(r, maxR);
+	// 	var pixel = currentPixel++;
+	// 	var x = pixel % DisplayWidth;
+	// 	var y = pixel / DisplayWidth;
+	// 	// y += 1;
+	//
+	// 	if (y >= 0 && y < DisplayHeight)
+	// 	{
+	// 		bitmap.SetPixel(x, y, new SKColor((byte)r, (byte)g, (byte)b));
+	// 	}
+	// 	else
+	// 	{
+	// 		_ = 1;
+	// 	}
+	// 	//bitmap.SetPixel(x, y, new SKColor((byte)r, (byte)g, (byte)b));
+	//
+	// 	currentPixel %= DisplayLength;
+	// }
 
 	protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
 	{
@@ -72,9 +101,17 @@ sealed partial class ScreenView : SKCanvasView, IDisplay
 		var vScale = dirtyRect.Height / DisplayHeight;
 		var hScale = dirtyRect.Width / DisplayWidth;
 		var scale = Math.Min(vScale, hScale);
-		canvas.Scale(scale);
+		
+		// Calculate centering offsets
+		float xOffset = (dirtyRect.Width - (DisplayWidth * scale)) / 2;
+		float yOffset = (dirtyRect.Height - (DisplayHeight * scale)) / 2;
+		
 		canvas.Clear(SKColors.White);
+		canvas.Save();
+		canvas.Translate(xOffset, yOffset);
+		canvas.Scale(scale);
 		canvas.DrawBitmap(bitmap, new SKPoint());
+		canvas.Restore();
 	}
 
 	public void RequestRefresh()
